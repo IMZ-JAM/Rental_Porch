9944d62eaf1779bf826d49ba05d64b5373e4a9c9
José Alonso <alonsomartinezjose12@gmail.com>
2023-09-26 23:18:39 -0600

    Login and Register añadidos

--- lib/presentation/register_screen.dart
+++ lib/presentation/register_screen.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:rental_porch_app/presentation/home_screen.dart';
-import 'package:rental_porch_app/presentation/register_screen.dart';
+import 'package:rental_porch_app/presentation/login_screen.dart';
 
-class LogInScreen extends StatefulWidget {
-  const LogInScreen({super.key});
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
 
   @override
-  State<LogInScreen> createState() => _LogInScreenState();
+  State<RegisterScreen> createState() => _RegisterScreenState();
 }
 
-class _LogInScreenState extends State<LogInScreen> {
+class _RegisterScreenState extends State<RegisterScreen> {
 
+  String _name = '';
   String _email = '';
   String _password = '';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
+    return Scaffold(
       body: Center(
         child: ListView(
           padding: const EdgeInsets.symmetric(horizontal: 60.0, vertical: 200.0),
@@ -24,10 +24,30 @@ class _LogInScreenState extends State<LogInScreen> {
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text('Bienvenidos a Rental-Porch', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),),
+                const Text('Unete a Rental-Porch', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),),
                 const SizedBox(height: 20,),
-                const Text('Inicio de sesion', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),),
+                const Text('REGISTRATE!', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),),
                 const SizedBox(height: 40,),
+                TextField(
+                  enableInteractiveSelection: false,
+                  autofocus: true,
+                  textCapitalization: TextCapitalization.characters,
+                  decoration: InputDecoration(
+                    hintText: 'Fernando Martínez',
+                    labelText: 'Nombre',
+                    suffixIcon: const Icon(
+                      Icons.verified_user,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)
+                    ),
+                  ),
+                  onSubmitted: (valor) {
+                    _name = valor;
+                    print('El email es $_name');
+                  },
+                ),
+                const SizedBox(height: 30,),
                 TextField(
                   enableInteractiveSelection: false,
                   autofocus: true,
@@ -67,11 +87,11 @@ class _LogInScreenState extends State<LogInScreen> {
                   },
                 ),
                 const SizedBox(height: 40,),
-                ElevatedButton(onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => const HomeScreen()));}, child: const Text('Iniciar sesion', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),)),
+                ElevatedButton(onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => const LogInScreen()));}, child: const Text('Registrate', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),)),
                 const SizedBox(height: 40,),
-                const Text('Aun no tienes cuenta?', style: TextStyle(color: Colors.black, fontSize: 22, fontWeight: FontWeight.bold),),
+                const Text('Ya tienes cuenta?', style: TextStyle(color: Colors.black, fontSize: 22, fontWeight: FontWeight.bold),),
                 const SizedBox(height: 20,),
-                ElevatedButton(onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => const RegisterScreen()));}, child: const Text('Registrate', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),))
+                ElevatedButton(onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => const LogInScreen()));}, child: const Text('Iniciar sesion', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),))
               ],
             ),
           ],
